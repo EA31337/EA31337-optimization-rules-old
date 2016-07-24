732dcfd298ebe080cdedc4cb5ef13fc6b102cdb1
kenorb <kenorb@users.noreply.github.com>
2016-07-24 02:37:53 +0100

    Fixes CI test failing on setting period in tester ini file

--- Scripts/test_rules.sh
+++ Scripts/test_rules.sh
@@ -3,12 +3,12 @@
 set -e
 [ "$TRACE" ] && set -x
 ROOT="$(git rev-parse --show-toplevel)"
-SETFILE="$ROOT/Sets/Lite/EURUSD/default/2000USD/10-spread/5-digits/2014/EA31337-Lite.set"
-TESTER_INI="$ROOT/_VM/conf/mt4-tester.ini"
-EA_INI="$ROOT/_VM/conf/ea.ini"
-cp -f "${SETFILE}" "${SETFILE}.test" && SETFILE="${SETFILE}.test"
-cp -f "${TESTER_INI}" "${TESTER_INI}.test" && TESTER_INI="${TESTER_INI}.test"
-cp -f "${EA_INI}" "${EA_INI}.test" && EA_INI="${EA_INI}.test"
+SETFILE="$ROOT/Sets/Lite/EURUSD/default/2000USD/10-spread/5-digits/2014/EA31337-Lite.set.test"
+TESTER_INI="$ROOT/_VM/conf/mt4-tester.ini.test"
+EA_INI="$ROOT/_VM/conf/ea.ini.test"
+cp -f "${SETFILE%.*}" "${SETFILE}"
+cp -f "${TESTER_INI%.*}" "${TESTER_INI}"
+cp -f "${EA_INI%.*}" "${EA_INI}"
 
 cd "$ROOT"
 . ./_VM/scripts/.funcs.inc.sh
